b9b1cbb9145e3e4214c2365d1dfad2c28d955b12
kartelmode <losevgg@gmail.com>
2023-03-27 21:23:42 +0300

    working repacking tests from zip-archive of polygon

--- routes/contest.go
+++ routes/contest.go
@@ -5,20 +5,19 @@ import (
 	"bytes"
 	"github.com/gofiber/fiber/v2"
 	"io"
-	"log"
 	"os"
 	"strings"
 )
 
 func ContestHandler(c *fiber.Ctx) error {
 	fileHeader, err := c.FormFile("Contest")
-	log.Println(fileHeader.Size)
 
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": err.Error()})
 	}
 
 	file, err := fileHeader.Open()
+	defer file.Close()
 
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": err.Error()})
@@ -35,18 +34,50 @@ func ContestHandler(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": err.Error()})
 	}
-	testsFile, _ := os.Create("tests.zip")
-	testsWriter := zip.NewWriter(testsFile)
-	defer testsWriter.Close()
-	defer testsFile.Close()
+
+	zipFile, err := os.Create("D:/Projects/BSUIR/backend/temp/tests.zip")
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": err.Error()})
+	}
+	testsWriter := zip.NewWriter(zipFile)
 
 	for _, f := range contestZip.File {
 		if f.FileInfo().IsDir() {
 			continue
 		}
 		if strings.HasPrefix(f.Name, "tests/") {
-			testsWriter.Copy(f)
+			err := CloneTest(f, testsWriter)
+			if err != nil {
+				testsWriter.Close()
+				zipFile.Close()
+				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": err.Error()})
+			}
 		}
 	}
-	return c.Status(fiber.StatusOK).SendFile("tests.zip")
+
+	c.SendFile("D:/Projects/BSUIR/backend/temp/tests.zip")
+
+	testsWriter.Close()
+	zipFile.Close()
+	os.Remove("D:/Projects/BSUIR/backend/temp/")
+	return c.SendStatus(fiber.StatusOK)
+}
+
+func CloneTest(f *zip.File, testsWriter *zip.Writer) error {
+	test, err := f.Open()
+	if err != nil {
+		return err
+	}
+	defer test.Close()
+	nameTest, _ := strings.CutPrefix(f.Name, "tests/")
+	zipTest, err := testsWriter.Create(nameTest)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(zipTest, test)
+	if err != nil {
+		return err
+	}
+	return nil
 }
